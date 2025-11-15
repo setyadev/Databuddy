584a1c70e92628d725e78591ddcf81657a06b43b
iza <59828082+izadoesdev@users.noreply.github.com>
2025-11-15 22:37:50 +0200

    cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,8 +6,6 @@ export default defineConfig({
         globals: true,
         environment: "node",
         coverage: {
-            provider: "v8",
-            reporter: ["text", "json", "html"],
             exclude: [
                 "node_modules/**",
                 "dist/**",
@@ -26,7 +24,6 @@ export default defineConfig({
         },
         mockReset: true,
         restoreMocks: true,
-        setupFiles: [],
     },
     resolve: {
         alias: {
